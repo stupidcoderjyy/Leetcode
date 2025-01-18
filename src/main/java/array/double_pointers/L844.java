6066c8baefe054c2ef2b55ce397eac432f38577c
stupid_coder_jyy <stupid_coder_jyy@163.com>
2025-01-18 15:49:11 +0800

    算法优化

--- src/main/java/array/double_pointers/L844.java
+++ src/main/java/array/double_pointers/L844.java
@@ -3,30 +3,33 @@ package array.double_pointers;
 public class L844 {
     public boolean backspaceCompare(String s, String t) {   //ERROR 算法设计错误
         final int N1 = s.length(), N2 = t.length();
-        int skipCount1 = 0, skipCount2 = 0;
         int check1 = N1 - 1, check2 = N2 - 1;
+        int unhandled = 0;  //未处理的'#'数量
         while (true) {
-            while (check1 >= 0) {
-                if (s.charAt(check1) == '#') {
-                    skipCount1++;
+            while (true) {
+                //检查连续的'#'
+                while (check1 >= 0 && s.charAt(check1) == '#') {
+                    unhandled++;
                     check1--;
-                } else if (skipCount1 > 0) {
-                    check1--;
-                    skipCount1--;
-                } else {
+                }
+                //处理结束
+                if (unhandled == 0) {
                     break;
                 }
+                //处理'#'
+                unhandled--;
+                check1--;
             }
-            while (check2 >= 0) {
-                if (t.charAt(check2) == '#') {
-                    skipCount2++;
+            while (true) {
+                while (check2 >= 0 && t.charAt(check2) == '#') {
+                    unhandled++;
                     check2--;
-                } else if (skipCount2 > 0) {
-                    check2--;
-                    skipCount2--;
-                } else {
+                }
+                if (unhandled == 0) {
                     break;
                 }
+                unhandled--;
+                check2--;
             }
             if (check1 < 0 || check2 < 0) {
                 return check1 < 0 && check2 < 0;
--- src/main/java/array/elements_remove/L26.java
+++ src/main/java/array/elements_remove/L26.java
@@ -1,7 +1,7 @@
 package array.elements_remove;
 
 public class L26 {
-    public int removeDuplicates(int[] nums) {     //ERROR 暴力解法
+    public int removeDuplicates(int[] nums) {
         int next = 0;
         int forward = 0;
         final int n = nums.length;
